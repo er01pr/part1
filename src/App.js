1a8f97cd774d128c3e561ed4949743a4e70e5cd0
Joshgripo <joshua.gripo@gmail.com>
2021-05-01 22:11:19 +0800

    Fixed Content  number of exercises

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ const Content = (props) => {
         {props.parts[0]} {props.exercises[0]}
       </p>
       <p>
-        {props.parts[1]} {props.exercises[0]}
+        {props.parts[1]} {props.exercises[1]}
       </p>
       <p>
-        {props.parts[2]} {props.exercises[0]}
+        {props.parts[2]} {props.exercises[2]}
       </p>
     </div>
   );
